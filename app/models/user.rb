335f7e1c6396508c848c527646a4b353a13a0b5d
inunokimoti <tc.202402.eri.nakamura@gmail.com>
2024-04-05 18:26:35 +0900

    バリテーションの再追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,8 @@ class User < ApplicationRecord
     validates :last_name_kana
     validates :first_name_kana
   end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
 end
