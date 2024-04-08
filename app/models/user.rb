f99aa235068215a4ed7e531cde6996ea49130d9c
inunokimoti <tc.202402.eri.nakamura@gmail.com>
2024-04-08 17:22:34 +0900

    アソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
 
+  has_many :items
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
@@ -18,8 +20,10 @@ class User < ApplicationRecord
     validates :last_name_kana
     validates :first_name_kana
   end
+
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）を使用してください' } do
     validates :last_name
     validates :first_name
   end
+
 end
