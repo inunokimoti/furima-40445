d1cf61f5e03c157a2dc8ad1cfcbf375d811a51da
inunokimoti <tc.202402.eri.nakamura@gmail.com>
2024-04-08 18:20:28 +0900

    ActiveHashの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :shipping_charge
+  belongs_to :from
+  belongs_to :transport_day
 end
